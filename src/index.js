c73a0524d2550a542873684554c2ec1f3ce87a58
keely <yuan.ql@foxmail.com>
2019-03-29 22:37:52 +0800

    test

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import React from './react'
 // React.render('123',document.getElementById('root'))
 
 // 原生的dom节点
-
 let btn = React.createElement('button', { id: '1' }, 'hello')
 let ele = React.createElement('div', { id: 'wrap' }, 'say', btn)
 React.render(ele, document.getElementById('root'))
