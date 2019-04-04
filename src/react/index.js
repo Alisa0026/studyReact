07210c6c5fb906246f720426501ea20120bbaa7e
keely <alisa19910026@gmail.com>
2019-04-04 15:50:15 +0800

    1.最简单的react渲染

--- src/react/index.js
+++ src/react/index.js
@@ -1,25 +1,11 @@
 // 用自己写的react实现最简单渲染效果
-import $ from 'jquery'
-import createUnitInstance from '../util/unit'
-import createElement from '../util/element'
-import Component from '../util/component'
-
 let React = {
-    nextRootIndex: 0,
-    render,
-    createElement,
-    Component
+    nextRootIndex:0,
+    render
 }
 
-function render(ele, container) {
-    // 扩展、通用，工厂方法，实例化组件
-    let unitInstance = createUnitInstance(ele);
-    // 获取对应的html
-    let unit = unitInstance.getUnit(React.nextRootIndex);
-    // 放到容器中
-    $(container).html(unit);
-    // 触发一个自定义事件 mounted，因为 getUnit中不同的组件都会监听 mounted 事件
-    $(document).trigger('mounted');
+function render(ele,container) {
+    container.innerHTML = `<span data-reactid='${React.nextRootIndex}'>${ele}</span>`
 }
 
 
